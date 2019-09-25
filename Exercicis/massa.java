eaedbd1824568cc60dbffd1b54b8524d053b8fbd
Carles Blanco <KBlancocebrian@gmail.com>
2019-09-25 19:23:51 +0200

    Exercici massa

--- Exercicis/massa.java
+++ Exercicis/massa.java
@@ -1,4 +1,4 @@
-public class massa{
+public class massa {
     public static void main(String[] args) {
         System.out.println("Introdueix la teva alçada");
         float alt = Float.parseFloat(System.console().readLine());
@@ -6,16 +6,15 @@ public class massa{
         int dos = 2;
         float altQuadrat = (float) Math.pow(alt, dos);
         float pes = Float.parseFloat(System.console().readLine());
-        float imc = pes/altQuadrat;
-        System.out.println("El teu index de massa corporal és "+imc+".");
+        float imc = pes / altQuadrat;
+        System.out.println("El teu index de massa corporal és " + imc + ".");
         if (imc <= 18.5) {
             System.out.println("Ep! Estas molt prim");
         } else if (imc <= 24.9) {
             System.out.println("El teu pes és saludable, segueix així :D");
         } else if (imc <= 29.9) {
             System.out.println("Ep! Tens sobrepes, hauries de vigilar la teva dieta");
-        }
-        else {
+        } else {
             System.out.println("Tens obesitat, hauries de anar al metge i començar a fer exercici");
         }
     }
